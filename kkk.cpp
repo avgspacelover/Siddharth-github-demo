aca1f1655897922cc6a94f1d4d6b241f835cfa31
Siddharth Ram <siddharth.asterisk@gmail.com>
2021-01-26 00:40:48 +0530

    Update kkk.cpp

--- kkk.cpp
+++ kkk.cpp
@@ -1,21 +1,11 @@
-//check_prime_number
-#include <iostream>
-#include <cmath>
-using namespace std;
-int main()
-{  int n;
-   cout<<"Enter Number\n";
-   cin>>n;  
-   int t=1;
-   for(int i=2;i<sqrt(n);i++){      
-      if(n%i==0){
-          cout<<"The number is composite & is divisible by "<<i<<endl;
-           t=2;          
-      }                          
-     } 
-   if(t==1){
-       cout<<" It's a PRIME number!\n";
-   }     
-
-   return 0;     
-}
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>studentsite</title>title>
+    </head>
+<body>
+    Duniya MC hain, Good Night!
+</body>
+</html>
